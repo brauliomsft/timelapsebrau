f39d9f07c4a2cc7c49e63e98cadf8158982dcedf
Alex Silva <alex.silva@poktapok.studio>
2020-05-29 14:33:17 -0500

    STATUS      Stable CHANGES:      * Index method refactor      * Added method "NexPage"      * implementation of continuation token      * Frist and Next options added to the view      * Number of the current page added to the view      * Name of the file added to the gallery

--- WebApp-Storage-DotNet/Controllers/HomeController.cs
+++ WebApp-Storage-DotNet/Controllers/HomeController.cs
@@ -48,145 +48,67 @@ namespace WebApp_Storage_DotNet.Controllers
         static CloudBlobClient blobClient;
         const string blobContainerName = "photos";
         static CloudBlobContainer blobContainer;
+		static BlobContinuationToken continuationToken = null;
+		static int count = 1;
 
-        /// <summary> 
-        /// Task<ActionResult> Index() 
-        /// Documentation References:  
-        /// - What is a Storage Account: http://azure.microsoft.com/en-us/documentation/articles/storage-whatis-account/ 
-        /// - Create a Storage Account: https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#create-an-azure-storage-account
-        /// - Create a Storage Container: https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#create-a-container
-        /// - List all Blobs in a Storage Container: https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#list-the-blobs-in-a-container
-        /// </summary> 
-        public async Task<ActionResult> Index()
+		/// <summary> 
+		/// Task<ActionResult> Index() 
+		/// Documentation References:  
+		/// - What is a Storage Account: http://azure.microsoft.com/en-us/documentation/articles/storage-whatis-account/ 
+		/// - Create a Storage Account: https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#create-an-azure-storage-account
+		/// - Create a Storage Container: https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#create-a-container
+		/// - List all Blobs in a Storage Container: https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#list-the-blobs-in-a-container
+		/// </summary> 
+		public async Task<ActionResult> Index(int page = 1)
         {
-            try
-            {
-                // Retrieve storage account information from connection string
-                // How to create a storage connection string - http://msdn.microsoft.com/en-us/library/azure/ee758697.aspx
-                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"].ToString());
 
-                // Create a blob client for interacting with the blob service.
-                blobClient = storageAccount.CreateCloudBlobClient();
-                blobContainer = blobClient.GetContainerReference(blobContainerName);
-                await blobContainer.CreateIfNotExistsAsync();
+			ViewBag.PageNumber = page;
+			continuationToken = page==1 ?  null: continuationToken;
+			List<Uri> blobs =await NextPage (page);
+			return View (blobs);
+		}
 
-                // To view the uploaded blob in a browser, you have two options. The first option is to use a Shared Access Signature (SAS) token to delegate  
-                // access to the resource. See the documentation links at the top for more information on SAS. The second approach is to set permissions  
-                // to allow public access to blobs in this container. Comment the line below to not use this approach and to use SAS. Then you can view the image  
-                // using: https://[InsertYourStorageAccountNameHere].blob.core.windows.net/webappstoragedotnet-imagecontainer/FileName 
-                await blobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+		public async Task<List<Uri>> NextPage(int page) {
+			try {
+				// Retrieve storage account information from connection string
+				// How to create a storage connection string - http://msdn.microsoft.com/en-us/library/azure/ee758697.aspx
+				CloudStorageAccount storageAccount = CloudStorageAccount.Parse (ConfigurationManager.AppSettings["StorageConnectionString"].ToString ());
 
-                // Gets all Cloud Block Blobs in the blobContainerName and passes them to teh view
-                List<Uri> allBlobs = new List<Uri>();
-                List<Uri> top100Blobs = new List<Uri>();
-                foreach (IListBlobItem blob in blobContainer.ListBlobs())
-                {
-                    if (blob.GetType() == typeof(CloudBlockBlob))
-                        allBlobs.Add(blob.Uri);
-                }
-                int count = allBlobs.Count;
+				// Create a blob client for interacting with the blob service.
+				blobClient = storageAccount.CreateCloudBlobClient ();
+				blobContainer = blobClient.GetContainerReference (blobContainerName);
+				await blobContainer.CreateIfNotExistsAsync ();
 
-                allBlobs.RemoveRange(0, count-100);
-             
-                return View(allBlobs);
-            }
-            catch (Exception ex)
-            {
-                ViewData["message"] = ex.Message;
-                ViewData["trace"] = ex.StackTrace;
-                return View("Error");
-            } 
-        }
-        /* REMOVE ALL PERMISSIONS TO DELETE
-        /// <summary> 
-        /// Task<ActionResult> UploadAsync() 
-        /// Documentation References:  
-        /// - UploadFromFileAsync Method: https://msdn.microsoft.com/en-us/library/azure/microsoft.windowsazure.storage.blob.cloudpageblob.uploadfromfileasync.aspx
-        /// </summary> 
-        [HttpPost]
-        public async Task<ActionResult> UploadAsync()
-        {
-            try
-            {
-                HttpFileCollectionBase files = Request.Files;
-                int fileCount = files.Count;
+				// To view the uploaded blob in a browser, you have two options. The first option is to use a Shared Access Signature (SAS) token to delegate  
+				// access to the resource. See the documentation links at the top for more information on SAS. The second approach is to set permissions  
+				// to allow public access to blobs in this container. Comment the line below to not use this approach and to use SAS. Then you can view the image  
+				// using: https://[InsertYourStorageAccountNameHere].blob.core.windows.net/webappstoragedotnet-imagecontainer/FileName 
+				await blobContainer.SetPermissionsAsync (new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
 
-                if (fileCount > 0)
-                {
-                    for (int i = 0; i < fileCount; i++)
-                    {
-                        CloudBlockBlob blob = blobContainer.GetBlockBlobReference(GetRandomBlobName(files[i].FileName));
-                        await blob.UploadFromFileAsync(files[i].FileName, FileMode.Open);
-                    }
-                }
-                return RedirectToAction("Index");
-            }
-            catch (Exception ex)
-            {
-                ViewData["message"] = ex.Message;
-                ViewData["trace"] = ex.StackTrace;
-                return View("Error");
-            }            
-        }
+				// Gets all Cloud Block Blobs in the blobContainerName and passes them to teh view
+				List<Uri> allBlobs = new List<Uri> ();
+				List<Uri> top100Blobs = new List<Uri> ();
+				List<IListBlobItem> blobs = new List<IListBlobItem> ();
 
-        /// <summary> 
-        /// Task<ActionResult> DeleteImage(string name) 
-        /// Documentation References:  
-        /// - Delete Blobs: https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#delete-blobs
-        /// </summary> 
-        [HttpPost]
-        public async Task<ActionResult> DeleteImage(string name)
-        {
-            try
-            {
-                Uri uri = new Uri(name);
-                string filename = Path.GetFileName(uri.LocalPath);
+				BlobResultSegment listingResult = await blobContainer.ListBlobsSegmentedAsync (string.Empty, true, BlobListingDetails.Metadata, 50, continuationToken, null, null);
+				continuationToken = listingResult.ContinuationToken;
+				blobs.AddRange (listingResult.Results);
+	
 
-                var blob = blobContainer.GetBlockBlobReference(filename);
-                await blob.DeleteIfExistsAsync();
+				foreach (IListBlobItem blob in blobs) {
+					if (blob.GetType () == typeof (CloudBlockBlob))
+						allBlobs.Add (blob.Uri);
+				}
+				ViewBag.AllBlobsCount = allBlobs.Count;
+				return allBlobs;
+			} catch (Exception ex) {
+				ViewData["message"] = ex.Message;
+				ViewData["trace"] = ex.StackTrace;
+				return null;
+			}
+		}
 
-                return RedirectToAction("Index");
-            }
-            catch (Exception ex)
-            {
-                ViewData["message"] = ex.Message;
-                ViewData["trace"] = ex.StackTrace;
-                return View("Error");
-            }
-        }
-
-        /// <summary> 
-        /// Task<ActionResult> DeleteAll(string name) 
-        /// Documentation References:  
-        /// - Delete Blobs: https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#delete-blobs
-        /// </summary> 
-        [HttpPost]
-        public async Task<ActionResult> DeleteAll()
-        {
-            try
-            {
-                foreach (var blob in blobContainer.ListBlobs())
-                {
-                    if (blob.GetType() == typeof(CloudBlockBlob))
-                    {
-                        await ((CloudBlockBlob)blob).DeleteIfExistsAsync();
-                    }
-                }
-
-                return RedirectToAction("Index");
-            }
-            catch (Exception ex)
-            {
-                ViewData["message"] = ex.Message;
-                ViewData["trace"] = ex.StackTrace;
-                return View("Error");
-            }
-        }
-        */
-        /// <summary> 
-        /// string GetRandomBlobName(string filename): Generates a unique random file name to be uploaded  
-        /// </summary> 
-        private string GetRandomBlobName(string filename)
+		private string GetRandomBlobName(string filename)
         {
             string ext = Path.GetExtension(filename);
             return string.Format("{0:10}_{1}{2}", DateTime.Now.Ticks, Guid.NewGuid(), ext);
